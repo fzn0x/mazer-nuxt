f32bc4343cc986eae2c8caecb36928e39bc22bb0
Muhammad Fauzan <developerfauzan@gmail.com>
2021-02-01 14:54:47 +0700

    chore(nuxt.config): add description content

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Mazer Nuxt - Nuxt Version of Mazer' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
