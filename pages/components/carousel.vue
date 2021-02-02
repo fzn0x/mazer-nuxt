c4adf574483b30c41676a424be3586915a6695bf
Muhammad Fauzan <developerfauzan@gmail.com>
2021-02-02 16:09:20 +0700

    feat: update breadcrumb-item dashboard link

--- pages/components/carousel.vue
+++ pages/components/carousel.vue
@@ -10,7 +10,7 @@
       <div class="col-12 col-md-6 order-md-2 order-first">
         <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
           <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+            <li class="breadcrumb-item"><router-link to="/">Dashboard</router-link></li>
             <li class="breadcrumb-item active" aria-current="page">Carousel</li>
           </ol>
         </nav>
