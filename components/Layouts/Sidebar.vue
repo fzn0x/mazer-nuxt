7bf29e860dc9bc478c3137090d727357d3ff5473
zuramai <saugi.dev@gmail.com>
2022-11-05 14:21:13 +0700

    fix: route error in sidebar

--- components/Layouts/Sidebar.vue
+++ components/Layouts/Sidebar.vue
@@ -1,8 +1,10 @@
 
 <script lang="ts" setup>
+import PerfectScrollbar from "perfect-scrollbar"
 import { useStore } from '~/store';
 
 const store = useStore()
+const route = useRoute()
 
 onMounted(() => {
     function slideToggle(t,e,o){0===t.clientHeight?j(t,e,o,!0):j(t,e,o)}function slideUp(t,e,o){j(t,e,o)}function slideDown(t,e,o){j(t,e,o,!0)}function j(t,e,o,i){void 0===e&&(e=400),void 0===i&&(i=!1),t.style.overflow="hidden",i&&(t.style.display="block");var p,l=window.getComputedStyle(t),n=parseFloat(l.getPropertyValue("height")),a=parseFloat(l.getPropertyValue("padding-top")),s=parseFloat(l.getPropertyValue("padding-bottom")),r=parseFloat(l.getPropertyValue("margin-top")),d=parseFloat(l.getPropertyValue("margin-bottom")),g=n/e,y=a/e,m=s/e,u=r/e,h=d/e;window.requestAnimationFrame(function l(x){void 0===p&&(p=x);var f=x-p;i?(t.style.height=g*f+"px",t.style.paddingTop=y*f+"px",t.style.paddingBottom=m*f+"px",t.style.marginTop=u*f+"px",t.style.marginBottom=h*f+"px"):(t.style.height=n-g*f+"px",t.style.paddingTop=a-y*f+"px",t.style.paddingBottom=s-m*f+"px",t.style.marginTop=r-u*f+"px",t.style.marginBottom=d-h*f+"px"),f>=e?(t.style.height="",t.style.paddingTop="",t.style.paddingBottom="",t.style.marginTop="",t.style.marginBottom="",t.style.overflow="",i||(t.style.display="none"),"function"==typeof o&&o()):window.requestAnimationFrame(l)})}
@@ -45,12 +47,10 @@ onMounted(() => {
     })
 
     // Perfect Scrollbar Init
-    if(typeof PerfectScrollbar.default == 'function') {
-        const container = document.querySelector(".sidebar-wrapper");
-        const ps = new PerfectScrollbar.default(container, {
-            wheelPropagation: false
-        });
-    }
+    const container = document.querySelector(".sidebar-wrapper");
+    const ps = new PerfectScrollbar(container, {
+        wheelPropagation: false
+    });
 
     // Scroll into active sidebar
     document.querySelector('.sidebar-item.active').scrollIntoView(false)
@@ -58,12 +58,12 @@ onMounted(() => {
     const subIsActive = (item)=>  {
         const paths = Array.isArray(item.submenu) ? item.submenu : [];
         return paths.some(path => {
-            return this.$route.path.indexOf(path.url) === 0;
+            return route.path.indexOf(path.url) === 0;
         });
     }
 
     const isActive = (url)=>  {
-        if(this.$route.path == url){
+        if(route.path == url){
             return true;
         }
         return false;
---
+++ store/data/sidebarItems.js
@@ -0,0 +1,124 @@
+export default [
+    {
+        "name": "Menu",
+        "isTitle": true
+    },
+    {
+        "name": "Dashboard",
+        "url": "/",
+        "icon": "grid-fill"
+    },
+    {
+      "name": "Components",
+      "key": "components",
+      "icon": "stack",
+      "submenu": [
+        {
+          "name": "Alert",
+          "url": "/components/alert"
+        },
+        {
+          "name": "Badge",
+          "url": "/components/badge"
+        },
+        {
+          "name": "Breadcrumb",
+          "url": "/components/breadcrumb"
+        },
+        {
+          "name": "Button",
+          "url": "/components/button"
+        },
+        {
+          "name": "Card",
+          "url": "/components/card"
+        },
+        {
+          "name": "Carousel",
+          "url": "/components/carousel"
+        },
+        {
+          "name": "Dropdown",
+          "url": "/components/dropdown"
+        },
+        {
+          "name": "List Group",
+          "url": "/components/list-group"
+        },
+        {
+          "name": "Modal",
+          "url": "/components/modal"
+        },
+        {
+          "name": "Navs",
+          "url": "/components/navs"
+        },
+        {
+          "name": "Pagination",
+          "url": "/components/pagination"
+        },
+        {
+          "name": "Progress",
+          "url": "/components/progress"
+        },
+        {
+          "name": "Spinner",
+          "url": "/components/spinner"
+        },
+        {
+          "name": "Tooltip",
+          "url": "/components/tooltip"
+        }
+      ]
+    },
+    {
+      "name": "Extra Components",
+      "key": "extra-component",
+      "icon": "collection-fill",
+      "submenu": [
+        {
+          "name": "Avatar",
+          "url": "/extra-components/avatar"
+        },
+        {
+          "name": "Sweet Alert",
+          "url": "/extra-components/sweetalert"
+        },
+        {
+          "name": "Toastify",
+          "url": "/extra-components/toastify"
+        },
+        {
+          "name": "Rating",
+          "url": "/extra-components/rating"
+        },
+        {
+          "name": "Divider",
+          "url": "/extra-components/divider"
+        }
+      ]
+    }, 
+    {
+      "name": "Layouts",
+      "key": "layout",
+      "icon": "grid-1x2-fill",
+      "submenu": [
+        {
+          "name": "Default Layout",
+          "url": "/layouts/default"
+        },
+        {
+          "name": "1 Column",
+          "url": "/layouts/1-column"
+        },
+        {
+          "name": "Vertical with Navbar",
+          "url": "/layouts/vertical"
+        },
+        // {
+        //   "name": "Horizontal Menu",
+        //   "url": "/layouts/horizontal"
+        // }
+      ]
+    },   
+];
