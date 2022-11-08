0ec2e6243910cb9935b63a7025755aeb3669482c
zuramai <saugi.dev@gmail.com>
2022-11-08 07:06:10 +0700

    fix: submenu toggle animation

--- components/Layouts/Sidebar/SidebarItem.vue
+++ components/Layouts/Sidebar/SidebarItem.vue
@@ -14,48 +14,47 @@ const props = defineProps<{
 }>()
 const route = useRoute()
 const sidebarItem = ref<HTMLElement>()
-function slideToggle(t: HTMLElement, e?: number, o?: number) { 0 === t.clientHeight ? j(t, e, o, !0) : j(t, e, o) } function slideUp(t, e, o) { j(t, e, o) } function slideDown(t, e, o) { j(t, e, o, !0) } function j(t: HTMLElement, e?: number, o?: number, i?: number) { void 0 === e && (e = 400), void 0 === i && (i = !1), t.style.overflow = "hidden", i && (t.style.display = "block"); var p, l = window.getComputedStyle(t), n = parseFloat(l.getPropertyValue("height")), a = parseFloat(l.getPropertyValue("padding-top")), s = parseFloat(l.getPropertyValue("padding-bottom")), r = parseFloat(l.getPropertyValue("margin-top")), d = parseFloat(l.getPropertyValue("margin-bottom")), g = n / e, y = a / e, m = s / e, u = r / e, h = d / e; window.requestAnimationFrame(function l(x) { void 0 === p && (p = x); var f = x - p; i ? (t.style.height = g * f + "px", t.style.paddingTop = y * f + "px", t.style.paddingBottom = m * f + "px", t.style.marginTop = u * f + "px", t.style.marginBottom = h * f + "px") : (t.style.height = n - g * f + "px", t.style.paddingTop = a - y * f + "px", t.style.paddingBottom = s - m * f + "px", t.style.marginTop = r - u * f + "px", t.style.marginBottom = d - h * f + "px"), f >= e ? (t.style.height = "", t.style.paddingTop = "", t.style.paddingBottom = "", t.style.marginTop = "", t.style.marginBottom = "", t.style.overflow = "", i || (t.style.display = "none"), "function" == typeof o && o()) : window.requestAnimationFrame(l) }) }
+const submenu = ref<HTMLElement>()
+const submenuOriginalHeight = ref(0)
+const submenuHeight = ref('auto')
+const isSubActive = ref(true)
+const isActive = computed(() => {
+    return route.path == props.item.url || (props.item.submenu && props.item.submenu.some((sub) => sub.url == route.path))
+})
 onMounted(() => {
-  sidebarItem.value?.querySelector('.sidebar-link')?.addEventListener('click', function(e) {
-      e.preventDefault();
-      
-      let submenu = sidebarItem.value?.querySelector<HTMLElement>('.submenu') 
-
-      if (submenu) {
-          if( submenu.classList.contains('active') ) submenu.style.display = "block"
-
-          if( submenu.style.display == "none" ) submenu?.classList.add('active')
-          else submenu.classList.remove('active')
-          slideToggle(submenu, 300)
-      }
-  })
+	if (submenu.value) {
+		submenuOriginalHeight.value = submenu.value.offsetHeight
+		
+		isSubActive.value = isActive.value
+		submenuHeight.value = (isSubActive.value ? submenuOriginalHeight.value : 0) + "px"
+	}
 })
 
-const isSubActive = computed(()=>  {
-    const paths = Array.isArray(props.item.submenu) ? props.item.submenu : [];
-    return paths.some(path => {
-        return route.path.indexOf(path.url) === 0;
-    });
-})
 
 const hasSub = computed(()=>{ 
-    if(props.item.hasOwnProperty("submenu")){
-        if(props.item.submenu.length > 0){
-            return true;
-        }
-    }
-    return false;
+	if(props.item.hasOwnProperty("submenu")){
+		if(props.item.submenu.length > 0){
+				return true;
+		}
+	}
+	return false;
 })
+const menuClick = (event: MouseEvent) => {
+	if(isSubActive.value)
+		submenuHeight.value = "0"
+	else submenuHeight.value = submenuOriginalHeight.value + "px"
+	isSubActive.value = !isSubActive.value
+}
 const isUrlActive = (url: string) => url == route.path
 </script>
 <template>
-<li class="sidebar-item" :class="{ 'active' : isSubActive, 'has-sub' : hasSub }" ref="sidebarItem"  >
+<li class="sidebar-item" :class="{ 'active' : isActive, 'has-sub' : hasSub }" ref="sidebarItem"  >
     <template v-if="hasSub">
-        <a class="sidebar-link" href="#">
+        <a class="sidebar-link" href="#" @click="menuClick">
             <i :class="`bi bi-${item.icon}`"></i>
             <span>{{ item.name }}</span>
         </a>
-        <ul class="submenu" :class="{ 'active' : isSubActive } ">
+        <ul class="submenu" :class="{ 'active' : isSubActive } " ref="submenu" :style="{height: submenuHeight}">
             <li v-for="(sub,i) in item.submenu" :key="i" class="submenu-item" :class="{'active': isUrlActive(sub.url)}">
                 <nuxt-link :to="sub.url">{{ sub.name }}</nuxt-link>
             </li>
@@ -69,3 +68,8 @@ const isUrlActive = (url: string) => url == route.path
     </template>
 </li>
 </template>
+<style>
+.submenu {
+    transition: all .5s !important;
+}
+</style>
