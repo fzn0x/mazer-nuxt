a3a780226c76bf0f855dd4b696770c05c41c1170
Hamid Reza Shakibi <lost.gost.1999@gmail.com>
2022-11-08 17:49:31 +0330

    fix: remove extra sidebar div

--- layouts/vertical.vue
+++ layouts/vertical.vue
@@ -5,16 +5,14 @@ import LayoutFooter from '~~/components/Layouts/LayoutFooter.vue';
 
 </script>
 <template>
-    <div id="app">
-        <div id="sidebar" class="active">
-            <LayoutSidebar></LayoutSidebar>
-        </div>
-        <div id="main" class="layout-navbar">
-            <VerticalHeader></VerticalHeader>
-            <div id="main-content">
-                <slot/>
-                <LayoutFooter></LayoutFooter>
-            </div>
-        </div>
+  <div id="app">
+    <LayoutSidebar></LayoutSidebar>
+    <div id="main" class="layout-navbar">
+      <VerticalHeader></VerticalHeader>
+      <div id="main-content">
+        <slot/>
+        <LayoutFooter></LayoutFooter>
+      </div>
     </div>
+  </div>
 </template>
